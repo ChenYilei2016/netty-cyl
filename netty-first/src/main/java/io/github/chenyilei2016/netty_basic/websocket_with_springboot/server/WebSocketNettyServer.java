8c2f21a41e0643256bb6c39cbbf3ad1f709bbe53
陈忆磊 <705029004@qq.com>
2024-07-09 13:41:38 +0800

    websocket base

--- netty-first/src/main/java/io/github/chenyilei2016/netty_basic/websocket_with_springboot/server/WebSocketNettyServer.java
+++ netty-first/src/main/java/io/github/chenyilei2016/netty_basic/websocket_with_springboot/server/WebSocketNettyServer.java
@@ -11,15 +11,18 @@ import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
 import java.net.InetSocketAddress;
 
+/**
+ * 可以自己处理websocket的upgrade ,  也可以WebSocketServerProtocolHandler
+ * {@link WebSocketServerProtocolHandler}
+ */
 @Component
 public class WebSocketNettyServer {
     //配置服务端NIO线程组
     private final EventLoopGroup parentGroup = new NioEventLoopGroup(); //NioEventLoopGroup extends MultithreadEventLoopGroup Math.max(1, SystemPropertyUtil.getInt("io.netty.eventLoopThreads", NettyRuntime.availableProcessors() * 2));
     private final EventLoopGroup childGroup = new NioEventLoopGroup();
-    private Channel channel;
+    private Channel futureChannel;
 
     public ChannelFuture bing(InetSocketAddress address) {
         ServerBootstrap serverBootstrap = new ServerBootstrap();
@@ -30,33 +33,22 @@ public class WebSocketNettyServer {
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     @Override
                     protected void initChannel(NioSocketChannel ch) throws Exception {
-                        /**
-                         *  channel.pipeline().addLast("http-codec", new HttpServerCodec());
-                         *                         channel.pipeline().addLast("aggregator", new HttpObjectAggregator(65536));
-                         *
-                         *                         java.lang.IllegalArgumentException: Duplicate handler name: http-codec
-                         * 	at io.netty.channel.DefaultChannelPipeline.checkDuplicateName(DefaultChannelPipeline.java:1066) [netty-all-4.1.37.Final.jar:4.1.37.Final]
-                         * 	at io.netty.channel.DefaultChannelPipeline.filterName(DefaultChannelPipeline.java:284) [netty-all-4.1.37.Final.jar:4.1.37.Final]
-                         * 	at io.netty.channel.DefaultChannelPipeline.addLast(DefaultChannelPipeline.java:204) [netty-all-4.1.37.Final.jar:4.1.37.Final]
-                         *
-                         * 	why ?? todo: xx???
-                         */
 
-                        channel.pipeline().addLast( new HttpServerCodec());
-                        channel.pipeline().addLast( new HttpObjectAggregator(65536));
-                        channel.pipeline().addLast(new WebSocketServerProtocolHandler("/websocket"));
-//                        channel.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
-                        channel.pipeline().addLast(new WebSocketServerHandler());
+                        ch.pipeline().addLast("http-codec", new HttpServerCodec());
+                        ch.pipeline().addLast("aggregator", new HttpObjectAggregator(65536));
+//                        ch.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+//                        ch.pipeline().addLast(new WebSocketServerProtocolHandler("/websocket"));
+                        ch.pipeline().addLast(new WebSocketServerHandler());
                     }
                 })
                 .bind(address);
-        this.channel = channelFuture.channel();
+        this.futureChannel = channelFuture.channel();
         return channelFuture;
     }
 
     public void destroy() {
-        if (null == channel) return;
-        channel.close();
+        if (null == futureChannel) return;
+        futureChannel.close();
         parentGroup.shutdownGracefully();
         childGroup.shutdownGracefully();
 
